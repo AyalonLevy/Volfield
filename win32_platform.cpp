0d9f4ab7b7fa0f32fb1545f1dfb26e2f80e0e002
AyalonLevy <levy.ayalon@gmail.com>
2026-06-23 10:04:22 +0300

    draw rectangle on screen
    add utils and renderer

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,6 +1,18 @@
+#include "utils.cpp"
 #include <windows.h>
 
-bool running = true;
+global_variable bool running = true;
+
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
@@ -9,8 +21,27 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 		case WM_CLOSE:
 		case WM_DESTROY: {
 			running = false;
-			break;
-		}
+		} break;
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int buffer_size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+
+			render_state.memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		} break;
 
 		default: {
 			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -32,7 +63,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// Create Window
 	HWND window = CreateWindow(window_class.lpszClassName, L"Volfield", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
-	
+	HDC hdc = GetDC(window);
+
 	while (running) {
 		// Input
 		MSG message;
@@ -42,8 +74,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		}
 
 		// Simulate
+		clear_screen(0xff5500);
+		draw_rect(0, 0, 20, 20, 0x00ff22);
 
 		// Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 
 	return 0;
